d7eab3e94eae337f832d5d2a5a9292db911501db
Rajesh Singh <rajeshrinet@gmail.com>
2020-03-26 07:02:09 +0000

    update examples

---
+++ examples/data/README.md
@@ -0,0 +1,14 @@
+Contact matrix data is obtained from 
+
+Projecting social contact matrices in 152 countries using contact surveys and demographic data (Prem-Jit-Cook)
+
+https://europepmc.org/backend/ptpmcrender.fcgi?accid=PMC5609774&blobtype=pdf
+
+Supplementary information file contains the contact matrices mu_ij for home, work, school and others and their sum, which is the total contact matrix. 
+
+Populations are aggregated into 5 year bins. The maximum age is 80. Therefore, there are 16 bins. 
+
+Data is Bayesian imputation combining POLYMOD, DHS, and ROW data. Empirical data exists for India, which is part of the DHS survey. Therefore, Bayesian imputation agrees very well with empirical data in this case. 
+
+Contact matrices may need to be adjusted to ensure reciprocity. 
+
--- pyross/models.pyx
+++ pyross/models.pyx
@@ -20,19 +20,20 @@ cdef class SIR:
     Is: symptomatic
     """
     cdef:
-        readonly int N, M
+        readonly int N, M, Tf
         readonly double alpha, beta, gamma, fsa
-        readonly np.ndarray rp0, Ni, drpdt, lld, CM
+        readonly np.ndarray rp0, Ni, drpdt, lld, CM, CC
     
-    def __init__(self, S0, Ia0, Is0, alpha, beta, gamma, fsa, M, Ni):
+    def __init__(self, S0, Ia0, Is0, alpha, beta, gamma, fsa, M, Ni, Tf):
 
         self.alpha = alpha 
         self.beta  = beta
         self.gamma = gamma 
         self.fsa   = fsa
 
-        self.N = np.sum(Ni)
-        self.M = M
+        self.N  = np.sum(Ni)
+        self.M  = M
+        self.Tf = Tf
 
         self.rp0   = np.zeros( 3*self.M, dtype=DTYPE)        # initial distribution
         self.Ni    = np.zeros( self.M, dtype=DTYPE)          # # people in each age-group
@@ -40,50 +41,48 @@ cdef class SIR:
         self.rp0[0:M]     = S0
         self.rp0[M:2*M]   = Ia0
         self.rp0[2*M:3*M] = Is0
-        
-        self.lld   = np.zeros( self.M, dtype=DTYPE)           # lambda matrix
-        self.CM    = np.zeros( (self.M, self.M), dtype=DTYPE) # contact matrix C
-        self.drpdt = np.zeros( 3*self.M, dtype=DTYPE)         # right hand side
+
+        self.CC    = np.zeros( (self.Tf, self.M**2), dtype=DTYPE)  # contact matrix C
+        self.CM    = np.zeros( (self.M, self.M), dtype=DTYPE)  # contact matrix C
+        self.drpdt = np.zeros( 3*self.M, dtype=DTYPE)              # right hand side
     
        
-    cdef rhs(self, rp):
+    cdef rhs(self, rp, tt):
+        self.CC = self.CM[int(tt),:].reshape(self.M, self.M)
         cdef: 
             int N=self.N, M=self.M, i, j
             double alpha=self.alpha, beta=self.beta, gamma=self.gamma, aa, bb
             double fsa=self.fsa
-            double [:] S  = rp[0:M]        
-            double [:] Ia = rp[M:2*M]       
-            double [:] Is = rp[2*M:3*M]       
-            double [:] Ni  = self.Ni       
-            double [:] ld  = self.lld       
-            double [:,:] CM = self.CM
-            double [:] X = self.drpdt        
+            double [:] S    = rp[0:M]        
+            double [:] Ia   = rp[M:2*M]       
+            double [:] Is   = rp[2*M:3*M]       
+            double [:] Ni   = self.Ni       
+            double [:] ld   = self.lld       
+            double [:,:] CM = self.CC
+            double [:] X    = self.drpdt        
 
-        
+
         for i in prange(M, nogil=True):
             bb=0
             for j in prange(M):
                  bb+= beta*(CM[i,j]*Ia[j]+fsa*CM[i,j]*Is[j])/Ni[j]
-            ld[i] = bb
-        for i in prange(M, nogil=True):
-            aa = ld[i]*S[i]
+            aa = bb*S[i]
             X[i]     = -aa
             X[i+M]   = alpha*aa     - gamma*Ia[i]
             X[i+2*M] = (1-alpha)*aa - gamma*Is[i]
         return
 
+
          
-    def simulate(self, Tf, CM, integrator='odeint', filename='this.mat'):
+    def simulate(self, CM, Nf, integrator='odeint', filename='this.mat'):
         from scipy.integrate import odeint
         from scipy.io import savemat
-
         self.CM = CM
-        time_points=np.linspace(0, Tf, Tf+1);  ## intervals at which output is returned by integrator. 
-        dt = time_points[1] - time_points[0]
+
+        time_points=np.linspace(0, self.Tf, Nf);  ## intervals at which output is returned by integrator. 
         
         def rhs0(rp, t):
-            self.rhs(rp)
-            #print (np.max(self.drpdt))
+            self.rhs(rp, t)
             return self.drpdt
             
         u = odeint(rhs0, self.rp0, time_points, mxstep=5000000)
