08f52282a8569f60b16862b8b59f203a72f8533f
Rajesh Singh <rajeshrinet@gmail.com>
2020-03-31 00:07:18 +0100

    clean-up

--- setup.py
+++ setup.py
@@ -6,40 +6,40 @@ from distutils.extension import Extension
 import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 
-#def checkOpenmpSupport():
-#    """ Adapted from https://stackoverflow.com/questions/16549893/programatically-testing-for-openmp-support-from-a-python-setup-script
-#    """ 
-#    ompTest = \
-#    r"""
-#    #include <omp.h>
-#    #include <stdio.h>
-#    int main() {
-#    #pragma omp parallel
-#    printf("Thread %d, Total number of threads %d\n", omp_get_thread_num(), omp_get_num_threads());
-#    }
-#    """
-#    tmpdir = tempfile.mkdtemp()
-#    curdir = os.getcwd()
-#    os.chdir(tmpdir)
-#
-#    filename = r'test.c'
-#    with open(filename, 'w') as file:
-#        file.write(ompTest)
-#    with open(os.devnull, 'w') as fnull:
-#        result = subprocess.call(['cc', '-fopenmp', filename],
-#                                 stdout=fnull, stderr=fnull)
-#
-#    os.chdir(curdir)
-#    shutil.rmtree(tmpdir) 
-#    if result == 0:
-#        return True
-#    else:
-#        return False
-#
-#if checkOpenmpSupport() == True:
-#    ompArgs = ['-fopenmp']
-#else:
-#    ompArgs = None 
+def checkOpenmpSupport():
+    """ Adapted from https://stackoverflow.com/questions/16549893/programatically-testing-for-openmp-support-from-a-python-setup-script
+    """ 
+    ompTest = \
+    r"""
+    #include <omp.h>
+    #include <stdio.h>
+    int main() {
+    #pragma omp parallel
+    printf("Thread %d, Total number of threads %d\n", omp_get_thread_num(), omp_get_num_threads());
+    }
+    """
+    tmpdir = tempfile.mkdtemp()
+    curdir = os.getcwd()
+    os.chdir(tmpdir)
+
+    filename = r'test.c'
+    with open(filename, 'w') as file:
+        file.write(ompTest)
+    with open(os.devnull, 'w') as fnull:
+        result = subprocess.call(['cc', '-fopenmp', filename],
+                                 stdout=fnull, stderr=fnull)
+
+    os.chdir(curdir)
+    shutil.rmtree(tmpdir) 
+    if result == 0:
+        return True
+    else:
+        return False
+
+if checkOpenmpSupport() == True:
+    ompArgs = ['-fopenmp']
+else:
+    ompArgs = None 
 
 
 setup(
@@ -53,8 +53,8 @@ setup(
     platforms='tested on LINUX',
     ext_modules=cythonize([ Extension("pyross/*", ["pyross/*.pyx"],
         include_dirs=[numpy.get_include()],
-#        extra_compile_args=ompArgs,
-#        extra_link_args=ompArgs 
+        extra_compile_args=ompArgs,
+        extra_link_args=ompArgs 
         )]),
     libraries=[],
     packages=['pyross'],
