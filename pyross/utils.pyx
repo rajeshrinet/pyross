aaae0460f73e1235eccd88973e38db439d38628c
Rajesh Singh <rajeshrinet@gmail.com>
2020-03-31 23:22:57 +0100

    clean-up

--- pyross/utils.pyx
+++ pyross/utils.pyx
@@ -6,10 +6,3 @@ from cython.parallel import prange
 cdef double PI = 3.1415926535
 from scipy.sparse import spdiags
 
-
-def addDelay(C, t, tC, tD, tW):
-   f1=0
-   for i in range(np.size(tD)):
-       dt = (t-tD[i])/tW[i]
-       f1 += 0.5*(1+np.tanh(dt))*C*tC[i]
-   return f1
