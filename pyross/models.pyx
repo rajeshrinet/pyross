de1889e4cccced4504ac91389b89dac303f87835
Rajesh Singh <rajeshrinet@gmail.com>
2020-04-03 21:02:41 +0100

    update all examples in the generic notation

--- pyross/models.pyx
+++ pyross/models.pyx
@@ -19,13 +19,14 @@ cdef class SIR:
     """
     cdef:
         readonly int N, M,
-        readonly double alpha, beta, gamma, fsa
+        readonly double alpha, beta, gIa, gIs, fsa
         readonly np.ndarray rp0, Ni, drpdt, lld, CM, CC
     
     def __init__(self, parameters, M, Ni):
         self.alpha = parameters.get('alpha')                    # fraction of asymptomatic infectives 
         self.beta  = parameters.get('beta')                     # infection rate 
-        self.gamma = parameters.get('gamma')                    # recovery rate
+        self.gIa   = parameters.get('gIa')                      # recovery rate of Ia
+        self.gIs   = parameters.get('gIa')                      # recovery rate of Is
         self.fsa   = parameters.get('fsa')                      # the self-isolation parameter 
 
         self.N     = np.sum(Ni)
@@ -40,8 +41,8 @@ cdef class SIR:
     cdef rhs(self, rp, tt):
         cdef: 
             int N=self.N, M=self.M, i, j
-            double alpha=self.alpha, beta=self.beta, gamma=self.gamma, aa, bb
-            double fsa=self.fsa, alphab = 1-self.alpha
+            double alpha=self.alpha, beta=self.beta, gIa=self.gIa, aa, bb
+            double fsa=self.fsa, alphab=1-self.alpha,gIs=self.gIs
             double [:] S    = rp[0:M]        
             double [:] Ia   = rp[M:2*M]       
             double [:] Is   = rp[2*M:3*M]       
@@ -56,8 +57,8 @@ cdef class SIR:
                  bb += beta*(CM[i,j]*Ia[j]+fsa*CM[i,j]*Is[j])/Ni[j]
             aa = bb*S[i]
             X[i]     = -aa
-            X[i+M]   = alpha *aa - gamma*Ia[i]
-            X[i+2*M] = alphab*aa - gamma*Is[i]
+            X[i+M]   = alpha *aa - gIa*Ia[i]
+            X[i+2*M] = alphab*aa - gIs*Is[i]
         return
 
          
@@ -79,7 +80,7 @@ cdef class SIR:
         #    #solver = odespy.RK4(rhs0)
         #    solver.set_initial_condition(self.rp0)
         #    u, time_points = solver.solve(time_points)
-        savemat(filename, {'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha, 'beta':self.beta,'gamma':self.gamma })
+        savemat(filename, {'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha, 'beta':self.beta,'gIa':self.gIa, 'gIs':self.gIs })
         return
         
 
@@ -97,14 +98,15 @@ cdef class SEIR:
     """
     cdef:
         readonly int N, M,
-        readonly double alpha, beta, gamma, gamaE, fsa
+        readonly double alpha, beta, gIa, gIs, gE, fsa
         readonly np.ndarray rp0, Ni, drpdt, lld, CM, CC
     
     def __init__(self, parameters, M, Ni):
         self.alpha = parameters.get('alpha')                    # fraction of asymptomatic infectives 
         self.beta  = parameters.get('beta')                     # infection rate 
-        self.gamma = parameters.get('gamma')                    # recovery rate
-        self.gamaE = parameters.get('gamaE')                     # inverse of the average incubation period
+        self.gIa   = parameters.get('gIa')                      # recovery rate of Ia
+        self.gIs   = parameters.get('gIs')                      # recovery rate of Is
+        self.gE    = parameters.get('gE')                       # recovery rate of E
         self.fsa   = parameters.get('fsa')                      # the self-isolation parameter 
 
         self.N     = np.sum(Ni)
@@ -119,8 +121,8 @@ cdef class SEIR:
     cdef rhs(self, rp, tt):
         cdef: 
             int N=self.N, M=self.M, i, j
-            double alpha=self.alpha, beta=self.beta, gamma=self.gamma, aa, bb
-            double fsa=self.fsa, gamaE=self.gamaE, ce1=self.gamaE*self.alpha, ce2=self.gamaE*(1-self.alpha)
+            double alpha=self.alpha, beta=self.beta, gIa=self.gIa, aa, bb
+            double fsa=self.fsa, gE=self.gE, ce1=self.gE*self.alpha, ce2=self.gE*(1-self.alpha)
             double [:] S    = rp[0:M]        
             double [:] E    = rp[M:2*M]       
             double [:] Ia   = rp[2*M:3*M]       
@@ -136,9 +138,9 @@ cdef class SEIR:
                  bb += beta*(CM[i,j]*Ia[j]+fsa*CM[i,j]*Is[j])/Ni[j]
             aa = bb*S[i]
             X[i]     = -aa
-            X[i+M]   = aa       - gamaE*E[i]
-            X[i+2*M] = ce1*E[i] - gamma*Ia[i]
-            X[i+3*M] = ce2*E[i] - gamma*Is[i]
+            X[i+M]   = aa       - gE*E[i]
+            X[i+2*M] = ce1*E[i] - gIa*Ia[i]
+            X[i+3*M] = ce2*E[i] - gIa*Is[i]
         return
 
          
@@ -160,6 +162,92 @@ cdef class SEIR:
         #    #solver = odespy.RK4(rhs0)
         #    solver.set_initial_condition(self.rp0)
         #    u, time_points = solver.solve(time_points)
-        savemat(filename, {'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha, 'beta':self.beta,'gamma':self.gamma })
+        savemat(filename, {'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha, 'beta':self.beta,'gIa':self.gIa,'gIs':self.gIs,'gE':self.gE})
         return
         
+
+
+
+@cython.wraparound(False)
+@cython.boundscheck(False)
+@cython.cdivision(True)
+@cython.nonecheck(False)
+cdef class SEAIR:
+    """
+    Susceptible, Exposed, Asymptomatic and infected, Infected, Recovered (SEAIR)
+    Ia: asymptomatic
+    Is: symptomatic
+    A : asymptotic and infectious
+    """
+    cdef:
+        readonly int N, M,
+        readonly double alpha, beta, gIa, gE, fsa
+        readonly np.ndarray rp0, Ni, drpdt, lld, CM, CC
+    
+    def __init__(self, parameters, M, Ni):
+        self.alpha = parameters.get('alpha')                    # fraction of asymptomatic infectives 
+        self.beta  = parameters.get('beta')                     # infection rate 
+        self.gIa   = parameters.get('gIa')                      # recovery rate of Ia
+        self.gIs   = parameters.get('gIs')                      # recovery rate of Is
+        self.gE    = parameters.get('gE')                       # recovery rate of E
+        self.gAA   = parameters.get('gE')                       # rate to go from A to Ia
+        self.gAS   = parameters.get('gE')                       # rate to go from A to Is
+        self.fsa   = parameters.get('fsa')                      # the self-isolation parameter 
+
+        self.N     = np.sum(Ni)
+        self.M     = M
+        self.Ni    = np.zeros( self.M, dtype=DTYPE)             # # people in each age-group
+        self.Ni    = Ni
+
+        self.CM    = np.zeros( (self.M, self.M), dtype=DTYPE)   # contact matrix C
+        self.drpdt = np.zeros( 4*self.M, dtype=DTYPE)           # right hand side
+    
+       
+    cdef rhs(self, rp, tt):
+        cdef: 
+            int N=self.N, M=self.M, i, j
+            double alpha=self.alpha, beta=self.beta, gIa=self.gIa, aa, bb
+            double fsa=self.fsa, gE=self.gE, ce1=self.gE*self.alpha, ce2=self.gE*(1-self.alpha)
+            double [:] S    = rp[0:M]        
+            double [:] E    = rp[M:2*M]       
+            double [:] Ia   = rp[2*M:3*M]       
+            double [:] Is   = rp[3*M:4*M]       
+            double [:] Ni   = self.Ni       
+            double [:] ld   = self.lld       
+            double [:,:] CM = self.CM
+            double [:] X    = self.drpdt        
+
+        for i in prange(M, nogil=True):
+            bb=0
+            for j in prange(M):
+                 bb += beta*(CM[i,j]*Ia[j]+fsa*CM[i,j]*Is[j])/Ni[j]
+            aa = bb*S[i]
+            X[i]     = -aa
+            X[i+M]   = aa       - gE*E[i]
+            X[i+2*M] = ce1*E[i] - gIa*Ia[i]
+            X[i+3*M] = ce2*E[i] - gIa*Is[i]
+        return
+
+         
+    def simulate(self, S0, E0, Ia0, Is0, contactMatrix, Tf, Nf, integrator='odeint', filename='this.mat'):
+        from scipy.integrate import odeint
+        from scipy.io import savemat
+        
+        def rhs0(rp, t):
+            self.rhs(rp, t)
+            self.CM = contactMatrix(t)
+            return self.drpdt
+            
+        time_points=np.linspace(0, Tf, Nf);  ## intervals at which output is returned by integrator. 
+        u = odeint(rhs0, np.concatenate((S0, E0, Ia0, Is0)), time_points, mxstep=5000000)
+        #elif integrator=='odespy-vode':
+        #    import odespy
+        #    solver = odespy.Vode(rhs0, method = 'bdf', atol=1E-7, rtol=1E-6, order=5, nsteps=10**6)
+        #    #solver = odespy.RKF45(rhs0)
+        #    #solver = odespy.RK4(rhs0)
+        #    solver.set_initial_condition(self.rp0)
+        #    u, time_points = solver.solve(time_points)
+        savemat(filename, {'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha,'beta':self.beta,'gIa':self.gIa,'gIs':self.gIs,'gE':self.gE,'gAA':self.gAA,'gAS':self.gAS})
+        return
+        
+
