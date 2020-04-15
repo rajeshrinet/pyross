b21b777160d8946438d0ed3e644859977a5d054c
Rajesh Singh <rajeshrinet@gmail.com>
2020-04-15 19:08:42 +0100

    start SIRS with influx of susceptibles and asymptomatics

--- pyross/deterministic.pyx
+++ pyross/deterministic.pyx
@@ -97,6 +97,102 @@ cdef class SIR:
 
 
 
+DTYPE   = np.float
+ctypedef np.float_t DTYPE_t
+@cython.wraparound(False)
+@cython.boundscheck(False)
+@cython.cdivision(True)
+@cython.nonecheck(False)
+cdef class SIRS:
+    """
+    Susceptible, Infected, Recovered (SIR)
+    Ia: asymptomatic
+    Is: symptomatic
+    """
+    cdef:
+        readonly int N, M,
+        readonly double alpha, beta, gIa, gIs, fsa
+        readonly np.ndarray rp0, Ni, drpdt, lld, CM, FM, CC
+
+    def __init__(self, parameters, M, Ni):
+        self.alpha = parameters.get('alpha')                    # fraction of asymptomatic infectives
+        self.beta  = parameters.get('beta')                     # infection rate
+        self.gIa   = parameters.get('gIa')                      # recovery rate of Ia
+        self.gIs   = parameters.get('gIa')                      # recovery rate of Is
+        self.fsa   = parameters.get('fsa')                      # the self-isolation parameter of symptomatics
+
+        self.sa   = parameters.get('sa')                      # daily arrival of new susceptibles 
+        self.iaa  = parameters.get('sa')                      # daily arrival of new asymptomatics
+
+
+        self.N     = np.sum(Ni)
+        self.M     = M
+        self.Ni    = np.zeros( self.M, dtype=DTYPE)             # # people in each age-group
+        self.Ni    = Ni
+
+        self.CM    = np.zeros( (self.M, self.M), dtype=DTYPE)   # contact matrix C
+        self.FM    = np.zeros( self.M, dtype = DTYPE)           # seed function F
+        self.drpdt = np.zeros( 4*self.M, dtype=DTYPE)           # right hand side
+
+
+    cdef rhs(self, rp, tt):
+        cdef:
+            int N=self.N, M=self.M, i, j
+            double alpha=self.alpha, beta=self.beta, gIa=self.gIa, aa, bb
+            double fsa=self.fsa, alphab=1-self.alpha,gIs=self.gIs
+            double sa=self.sa, iaa=self.iaa
+            double [:] S    = rp[0  :M]
+            double [:] Ia   = rp[M  :2*M]
+            double [:] Is   = rp[2*M:3*M]
+            double [:] Ni   = self.Ni
+            double [:] ld   = self.lld
+            double [:,:] CM = self.CM
+            double [:]   FM = self.FM
+            double [:] X    = self.drpdt
+
+        for i in prange(M, nogil=True):
+            bb=0
+            for j in prange(M):
+                 bb += beta*(CM[i,j]*Ia[j]+fsa*CM[i,j]*Is[j])/Ni[j]
+            aa = bb*S[i]
+            X[i]     = -aa - FM[i] + sa
+            X[i+M]   = alpha *aa - gIa*Ia[i] + alpha * FM[i] + iaa
+            X[i+2*M] = alphab*aa - gIs*Is[i] + alphab* FM[i]
+            X[i+3*M] = sa + iaa
+        return
+
+
+    def simulate(self, S0, Ia0, Is0, contactMatrix, Tf, Nf, integrator='odeint', filename='None', seedRate=None):
+        from scipy.integrate import odeint
+
+        def rhs0(rp, t):
+            if None != seedRate :
+                self.FM = seedRate(t)
+            else :
+                self.FM = np.zeros( self.M, dtype = DTYPE)
+            self.rhs(rp, t)
+            self.CM = contactMatrix(t)
+            return self.drpdt
+
+        time_points=np.linspace(0, Tf, Nf);  ## intervals at which output is returned by integrator.
+        u = odeint(rhs0, np.concatenate((S0, Ia0, Is0, N0)), time_points, mxstep=5000000)
+        #elif integrator=='odespy-vode':
+        #    import odespy
+        #    solver = odespy.Vode(rhs0, method = 'bdf', atol=1E-7, rtol=1E-6, order=5, nsteps=10**6)
+        #    #solver = odespy.RKF45(rhs0)
+        #    #solver = odespy.RK4(rhs0)
+        #    solver.set_initial_condition(self.rp0)
+        #    u, time_points = solver.solve(time_points)
+
+        if filename=='None':
+            data={'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha, 'beta':self.beta,'gIa':self.gIa, 'gIs':self.gIs }
+        else:
+            data={'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha, 'beta':self.beta,'gIa':self.gIa, 'gIs':self.gIs }
+            from scipy.io import savemat
+            savemat(filename, {'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha, 'beta':self.beta,'gIa':self.gIa, 'gIs':self.gIs })
+        return data
+
+
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.cdivision(True)
