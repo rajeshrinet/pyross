9135cf2f9cfd72f3dff6665c707aaa2eb6bb2d0c
Rajesh Singh <rajeshrinet@gmail.com>
2020-04-22 18:43:50 +0100

    SEAI5R model

--- pyross/deterministic.pyx
+++ pyross/deterministic.pyx
@@ -61,7 +61,6 @@ cdef class SIR:
 
 
     def simulate(self, S0, Ia0, Is0, contactMatrix, Tf, Nf, Ti=0, integrator='odeint', seedRate=None):
-        from scipy.integrate import odeint
 
         def rhs0(rp, t):
             self.CM = contactMatrix(t)
@@ -73,6 +72,7 @@ cdef class SIR:
             return self.drpdt
 
         if integrator=='odeint':
+            from scipy.integrate import odeint
             time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
             u = odeint(rhs0, np.concatenate((S0, Ia0, Is0)), time_points, mxstep=100000)
         else:
@@ -169,7 +169,6 @@ cdef class SIRS:
 
 
     def simulate(self, S0, Ia0, Is0, contactMatrix, Tf, Nf, Ti=0, integrator='odeint', seedRate=None):
-        from scipy.integrate import odeint
 
         def rhs0(rp, t):
             self.CM = contactMatrix(t)
@@ -177,6 +176,7 @@ cdef class SIRS:
             return self.drpdt
 
         if integrator=='odeint':
+            from scipy.integrate import odeint
             time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
             u = odeint(rhs0, np.concatenate((S0, Ia0, Is0, self.Ni)), time_points, mxstep=5000000)
         else:
@@ -254,7 +254,6 @@ cdef class SEIR:
 
 
     def simulate(self, S0, E0, Ia0, Is0, contactMatrix, Tf, Nf, Ti=0, integrator='odeint', seedRate=None):
-        from scipy.integrate import odeint
 
         def rhs0(rp, t):
             self.CM = contactMatrix(t)
@@ -266,6 +265,7 @@ cdef class SEIR:
             return self.drpdt
 
         if integrator=='odeint':
+            from scipy.integrate import odeint
             time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
             u = odeint(rhs0, np.concatenate((S0, E0, Ia0, Is0)), time_points, mxstep=5000000)
         else:
@@ -405,7 +405,6 @@ cdef class SEI5R:
 
 
     def simulate(self, S0, E0, Ia0, Is0, Ih0, Ic0, Im0, contactMatrix, Tf, Nf, Ti=0, integrator='odeint', seedRate=None):
-        from scipy.integrate import odeint
 
         def rhs0(rp, t):
             self.CM = contactMatrix(t)
@@ -413,6 +412,7 @@ cdef class SEI5R:
             return self.drpdt
 
         if integrator=='odeint':
+            from scipy.integrate import odeint
             time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
             u = odeint(rhs0, np.concatenate((S0, E0, Ia0, Is0, Ih0, Ic0, Im0, self.Ni)), time_points, mxstep=5000000)
         else:
@@ -680,7 +680,7 @@ cdef class SEAIR:
         for i in range(M):
             bb=0
             for j in range(M):
-                 bb += beta*CM[i,j]*(Ia[j]+fsa*Is[j])/Ni[j]
+                 bb += beta*CM[i,j]*(A[j]+Ia[j]+fsa*Is[j])/Ni[j]
             aa = bb*S[i]
             X[i]     = -aa - FM[i]
             X[i+M]   =  aa      - gE       *E[i] + FM[i]
@@ -691,7 +691,6 @@ cdef class SEAIR:
 
 
     def simulate(self, S0, E0, A0, Ia0, Is0, contactMatrix, Tf, Nf, Ti=0, integrator='odeint', seedRate=None):
-        from scipy.integrate import odeint
 
         def rhs0(rp, t):
             self.CM = contactMatrix(t)
@@ -703,6 +702,7 @@ cdef class SEAIR:
             return self.drpdt
 
         if integrator=='odeint':
+            from scipy.integrate import odeint
             time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
             u = odeint(rhs0, np.concatenate((S0, E0, A0, Ia0, Is0)), time_points, mxstep=5000000)
         else:
@@ -785,7 +785,7 @@ cdef class SEAIRQ:
         for i in range(M):
             bb=0
             for j in range(M):
-                 bb += beta*CM[i,j]*(Ia[j]+fsa*Is[j])/Ni[j]
+                 bb += beta*CM[i,j]*(A[j]+Ia[j]+fsa*Is[j])/Ni[j]
             aa = bb*S[i]
             X[i]     = -aa      - tS          *S[i] - FM[i]
             X[i+M]   =  aa      - (gE+tE)     *E[i] + FM[i]
@@ -796,7 +796,6 @@ cdef class SEAIRQ:
 
 
     def simulate(self, S0, E0, A0, Ia0, Is0, contactMatrix, Tf, Nf, Ti=0, integrator='odeint', seedRate=None):
-        from scipy.integrate import odeint
 
         def rhs0(rp, t):
             self.CM = contactMatrix(t)
@@ -808,6 +807,7 @@ cdef class SEAIRQ:
             return self.drpdt
 
         if integrator=='odeint':
+            from scipy.integrate import odeint
             time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
             u = odeint(rhs0, np.concatenate((S0, E0, A0, Ia0, Is0)), time_points, mxstep=5000000)
         else:
@@ -821,3 +821,157 @@ cdef class SEAIRQ:
 
         data={'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha,'beta':self.beta,'gIa':self.gIa,'gIs':self.gIs,'gE':self.gE,'gAA':self.gAA,'gAS':self.gAS,'tS':self.tS,'tE':self.tE,'tIa':self.tIa,'tIs':self.tIs}
         return data
+
+
+
+
+@cython.wraparound(False)
+@cython.boundscheck(False)
+@cython.cdivision(True)
+@cython.nonecheck(False)
+cdef class SEAI5R:
+    """
+    Susceptible, Exposed, Infected, Recovered (SEIR)
+    The infected class has 5 groups:
+    * Ia: asymptomatic
+    * Is: symptomatic
+    * Ih: hospitalized
+    * Ic: ICU
+    * Im: Mortality
+
+    S  ---> E
+    E  ---> Ia, Is
+    Ia ---> R
+    Is ---> Ih, R
+    Ih ---> Ic, R
+    Ic ---> Im, R
+    """
+    cdef:
+        readonly int N, M,
+        readonly double alpha, beta, gE, gIa, gIs, gIh, gIc, fsa, fh
+        readonly np.ndarray rp0, Ni, drpdt, CM, FM, CC, sa, iaa, hh, cc, mm
+
+    def __init__(self, parameters, M, Ni):
+        self.alpha = parameters.get('alpha')                    # fraction of asymptomatic infectives
+        self.beta  = parameters.get('beta')                     # infection rate
+        self.gE    = parameters.get('gE')                       # recovery rate of E class
+        self.gA    = parameters.get('gA')                       # recovery rate of A class
+        self.gIa   = parameters.get('gIa')                      # recovery rate of Ia
+        self.gIs   = parameters.get('gIs')                      # recovery rate of Is
+        self.gIh   = parameters.get('gIh')                      # recovery rate of Is
+        self.gIc   = parameters.get('gIc')                      # recovery rate of Ih
+        self.fsa   = parameters.get('fsa')                      # the self-isolation parameter of symptomatics
+        self.fh    = parameters.get('fh')                       # the self-isolation parameter of hospitalizeds
+
+        sa         = parameters.get('sa')                       # daily arrival of new susceptibles
+        hh         = parameters.get('hh')                       # hospital
+        cc         = parameters.get('cc')                       # ICU
+        mm         = parameters.get('mm')                       # mortality
+
+        self.N     = np.sum(Ni)
+        self.M     = M
+        self.Ni    = np.zeros( self.M, dtype=DTYPE)             # # people in each age-group
+        self.Ni    = Ni
+
+        self.CM    = np.zeros( (self.M, self.M), dtype=DTYPE)   # contact matrix C
+        self.drpdt = np.zeros( 9*self.M, dtype=DTYPE)           # right hand side
+
+        self.sa    = np.zeros( self.M, dtype = DTYPE)
+        if np.size(sa)==1:
+            self.sa = sa*np.ones(M)
+        elif np.size(sa)==M:
+            self.sa= sa
+        else:
+            print('sa can be a number or an array of size M')
+
+        self.hh    = np.zeros( self.M, dtype = DTYPE)
+        if np.size(hh)==1:
+            self.hh = hh*np.ones(M)
+        elif np.size(hh)==M:
+            self.hh= hh
+        else:
+            print('hh can be a number or an array of size M')
+
+        self.cc    = np.zeros( self.M, dtype = DTYPE)
+        if np.size(cc)==1:
+            self.cc = cc*np.ones(M)
+        elif np.size(cc)==M:
+            self.cc= cc
+        else:
+            print('cc can be a number or an array of size M')
+
+        self.mm    = np.zeros( self.M, dtype = DTYPE)
+        if np.size(mm)==1:
+            self.mm = mm*np.ones(M)
+        elif np.size(mm)==M:
+            self.mm= mm
+        else:
+            print('mm can be a number or an array of size M')
+
+
+    cdef rhs(self, rp, tt):
+        cdef:
+            int N=self.N, M=self.M, i, j
+            double alpha=self.alpha, beta=self.beta, aa, bb
+            double fsa=self.fsa, fh=self.fh, alphab=1-self.alpha, gE=self.gE, gA=self.gA
+            double gIs=self.gIs, gIa=self.gIa, gIh=self.gIh, gIc=self.gIh
+            double ca1=self.gA*self.alpha, ca2=self.gA*(1-self.alpha)
+            double [:] S    = rp[0  :M]
+            double [:] E    = rp[M  :2*M]
+            double [:] A    = rp[2*M:3*M]
+            double [:] Ia   = rp[3*M:4*M]
+            double [:] Is   = rp[4*M:5*M]
+            double [:] Ih   = rp[5*M:6*M]
+            double [:] Ic   = rp[6*M:7*M]
+            double [:] Im   = rp[7*M:8*M]
+            double [:] Ni   = rp[8*M:9*M]
+            double [:,:] CM = self.CM
+            double [:] sa   = self.sa       #sa is rate of additional/removal of population by birth etc
+            double [:] hh   = self.hh
+            double [:] cc   = self.cc
+            double [:] mm   = self.mm
+            double [:] X    = self.drpdt
+
+        for i in range(M):
+            bb=0
+            for j in range(M):
+                 bb += beta*CM[i,j]*(A[i]+Ia[j]+fsa*Is[j]+fh*Ih[j])/Ni[j]
+            aa = bb*S[i]
+            X[i]     = -aa + sa[i]                       
+            X[i+M]   = aa  - gE*E[i]                     
+            X[i+2*M] = gE*E[i]  - gA*A[i]                     
+            X[i+3*M] = ca1*A[i] - gIa*Ia[i]              
+            X[i+4*M] = ca2*A[i] - gIs*Is[i]              
+            X[i+5*M] = gIs*hh[i]*Is[i] - gIh*Ih[i]       
+            X[i+6*M] = gIh*cc[i]*Ih[i] - gIc*Ic[i]       
+            X[i+7*M] = gIc*mm[i]*Ic[i]                   
+            X[i+8*M] = sa[i] - gIc*mm[i]*Im[i]           
+        return
+
+
+    def simulate(self, S0, E0, A0, Ia0, Is0, Ih0, Ic0, Im0, contactMatrix, Tf, Nf, Ti=0, integrator='odeint', seedRate=None):
+
+        def rhs0(rp, t):
+            self.CM = contactMatrix(t)
+            self.rhs(rp, t)
+            return self.drpdt
+
+        if integrator=='odeint':
+            from scipy.integrate import odeint
+            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
+            u = odeint(rhs0, np.concatenate((S0, E0, Ia0, Is0, Ih0, Ic0, Im0, self.Ni)), time_points, mxstep=5000000)
+        else:
+            import odespy
+            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
+            solver = odespy.Vode(rhs0, method = 'bdf', atol=1E-7, rtol=1E-6, order=5, nsteps=10**6)
+            #solver = odespy.RKF45(rhs0)
+            #solver = odespy.RK4(rhs0)
+            solver.set_initial_condition(np.concatenate((S0, E0, A0, Ia0, Is0, Ih0, Ic0, Im0, self.Ni)))
+            u, time_points = solver.solve(time_points)
+
+        data={'X':u, 't':time_points, 'N':self.N, 'M':self.M,'alpha':self.alpha, 'beta':self.beta,'gIa':self.gIa,'gIs':self.gIs,'gE':self.gE}
+        return data
+
+
+
+
