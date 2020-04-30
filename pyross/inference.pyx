971f852a0371ee62f4601148f09b3f84739e229c
Paul Rohrbach <pbr28@cam.ac.uk>
2020-04-30 18:57:31 +0200

    Added missing file for previous commit

--- pyross/inference.pyx
+++ pyross/inference.pyx
@@ -10,7 +10,8 @@ cimport cython
 import pyross.deterministic
 cimport pyross.deterministic
 import pyross.contactMatrix
-from pyross.utils import BoundedSteps, minimisation
+from pyross.utils import BoundedSteps
+from pyross.utils_python import minimization
 from libc.math cimport sqrt, log, INFINITY
 cdef double PI = 3.14159265359
 
@@ -55,6 +56,20 @@ cdef class SIR_type:
         self.flat_indices2 = np.ravel_multi_index((c, r), (self.dim, self.dim))
 
 
+    def _inference_to_minimize(self, params, grad=0, bounds=None, eps=None, beta_rescale=None, x=None, Tf=None, Nf=None,
+                               contactMatrix=None, a=None, scale=None):
+        if (params>(bounds[:, 1]-eps)).all() or (params < (bounds[:,0]+eps)).all():
+            return INFINITY
+
+        local_params = params.copy()
+        local_params[1] /= beta_rescale
+        parameters = self.make_params_dict(local_params)
+        self.set_params(parameters)
+        model = self.make_det_model(parameters)
+        minus_logp = self.obtain_log_p_for_traj(x, Tf, Nf, model, contactMatrix)
+        minus_logp -= np.sum(gamma.logpdf(local_params, a, scale=scale))
+        return minus_logp
+        
     def inference(self, guess, stds, x, Tf, Nf, contactMatrix, beta_rescale=1, bounds=None, niter=2, verbose=False, 
                   ftol=1e-6, eps=1e-5, global_max_iter=100, local_max_iter=100, global_ftol_factor=10.,
                   enable_global=True, enable_local=True, cma_processes=0, cma_population=16, cma_stds=None):
@@ -79,17 +94,6 @@ cdef class SIR_type:
             size of steps taken by L-BFGS-B algorithm for the calculation of Hessian
         '''
         a, scale = pyross.utils.make_gamma_dist(guess, stds)
-        def to_minimize(params, grad=0):
-            if (params>(bounds[:, 1]-eps)).all() or (params < (bounds[:,0]+eps)).all():
-                return INFINITY
-            local_params = params.copy()
-            local_params[1] /= beta_rescale
-            parameters = self.make_params_dict(local_params)
-            self.set_params(parameters)
-            model = self.make_det_model(parameters)
-            minus_logp = self.obtain_log_p_for_traj(x, Tf, Nf, model, contactMatrix)
-            minus_logp -= np.sum(gamma.logpdf(local_params, a, scale=scale))
-            return minus_logp
 
         # make bounds if it does not exist and rescale
         if bounds is None:
@@ -100,21 +104,16 @@ cdef class SIR_type:
         guess[1] *= beta_rescale
         bounds[1] *= beta_rescale
 
-#        options={'eps': eps, 'ftol': ftol, 'disp': verbose}
-#        minimizer_kwargs = {'method':'L-BFGS-B', 'bounds': bounds, 'options': options}
-#        if verbose:
-#            def callback(params):
-#                print('parameters:', params)
-#            minimizer_kwargs['callback']= callback
-#        take_step = BoundedSteps(bounds)
-#        res = basinhopping(to_minimize, guess, niter=niter,
-#                            minimizer_kwargs=minimizer_kwargs,
-#                            take_step=take_step, disp=verbose)
-        
-        res = minimisation(to_minimize, guess, bounds, ftol=ftol, global_max_iter=global_max_iter, local_max_iter=local_max_iter,
-                           global_ftol_factor=global_ftol_factor, enable_global=enable_global, enable_local=enable_local,
-                           cma_processes=cma_processes, cma_population=cma_population, cma_stds=cma_stds,
-                           verbose=verbose)
+        if cma_stds is None:
+            # Use prior standard deviations here (todo: is this a good value?)
+            cma_stds = 1.0/3.0 * stds
+            
+        minimize_args={'bounds':bounds, 'eps':eps, 'beta_rescale':beta_rescale, 'x':x, 'Tf':Tf, 'Nf':Nf, 
+                         'contactMatrix':contactMatrix, 'a':a, 'scale':scale}
+        res = minimization(self._inference_to_minimize, guess, bounds, ftol=ftol, global_max_iter=global_max_iter, 
+                           local_max_iter=local_max_iter, global_ftol_factor=global_ftol_factor, 
+                           enable_global=enable_global, enable_local=enable_local, cma_processes=cma_processes, 
+                           cma_population=cma_population, cma_stds=cma_stds, verbose=verbose, args_dict=minimize_args)
         estimates = res[0]
         estimates[1] /= beta_rescale
         return estimates
@@ -215,6 +214,21 @@ cdef class SIR_type:
         minus_logp = self.obtain_log_p_for_traj(x, Tf, Nf, model, contactMatrix)
         return minus_logp
 
+    def _latent_inference_to_minimize(self, params, grad = 0, bounds=None, eps=None, param_dim=None, rescale_factor=None, 
+                beta_rescale=None, obs=None, fltr=None, Tf=None,   Nf=None, contactMatrix=None, a=None, scale=None):
+        if (params>(bounds[:, 1]-eps)).all() or (params < (bounds[:,0]+eps)).all():
+            return INFINITY
+        x0 =  params[param_dim:]/rescale_factor
+
+        local_params = params.copy()
+        local_params[1] /= beta_rescale
+        parameters = self.make_params_dict(local_params[:param_dim])
+        self.set_params(parameters)
+        model = self.make_det_model(parameters)
+        minus_logp = self.obtain_log_p_for_traj_red(x0, obs[1:], fltr, Tf, Nf, model, contactMatrix)
+        minus_logp -= np.sum(gamma.logpdf(local_params, a, scale=scale))
+        return minus_logp
+
     def latent_inference(self, np.ndarray guess, np.ndarray stds, np.ndarray obs, np.ndarray fltr,
                             double Tf, Py_ssize_t Nf, contactMatrix, np.ndarray bounds,
                             beta_rescale=1, verbose=False, Py_ssize_t niter=1,
@@ -256,35 +270,17 @@ cdef class SIR_type:
         bounds[param_dim:, :] *= rescale_factor
         bounds[1, :] *= beta_rescale
         a, scale = pyross.utils.make_gamma_dist(guess, stds)
-        def to_minimize(params, grad = 0):
-            if (params>(bounds[:, 1]-eps)).all() or (params < (bounds[:,0]+eps)).all():
-                return INFINITY
-            x0 =  params[param_dim:]/rescale_factor
-            # NLopt does not like us changing params here, so we just make a copy.
-            local_params = params.copy()
-            local_params[1] /= beta_rescale
-            parameters = self.make_params_dict(local_params[:param_dim])
-            self.set_params(parameters)
-            model = self.make_det_model(parameters)
-            minus_logp = self.obtain_log_p_for_traj_red(x0, obs[1:], fltr, Tf, Nf, model, contactMatrix)
-            minus_logp -= np.sum(gamma.logpdf(local_params, a, scale=scale))
-            return minus_logp
 
-#        options={'eps': eps, 'ftol': ftol, 'disp': verbose}
-#        minimizer_kwargs = {'method':'L-BFGS-B', 'bounds': bounds, 'options': options}
-#        if verbose:
-#            def callback(params):
-#                print('parameters:', params[:-self.dim])
-#            minimizer_kwargs['callback'] = callback
-#        take_step = BoundedSteps(bounds)
-#        res = basinhopping(to_minimize, guess, niter=niter,
-#                            minimizer_kwargs=minimizer_kwargs,
-#                            take_step=take_step, disp=verbose)
-
-        res = minimisation(to_minimize, guess, bounds, ftol=ftol, global_max_iter=global_max_iter, local_max_iter=local_max_iter,
-                           global_ftol_factor=global_ftol_factor, enable_global=enable_global, enable_local=enable_local,
-                           cma_processes=cma_processes, cma_population=cma_population, cma_stds=cma_stds,
-                           verbose=verbose)
+        if cma_stds is None:
+            # Use prior standard deviations here
+            cma_stds = 1.0/3.0 * stds
+            
+        minimize_args = {'bounds':bounds, 'eps':eps, 'param_dim':param_dim, 'rescale_factor':rescale_factor, 'beta_rescale':beta_rescale, 
+                         'obs':obs, 'fltr':fltr, 'Tf':Tf, 'Nf':Nf, 'contactMatrix':contactMatrix, 'a':a, 'scale':scale}
+        res = minimization(self._latent_inference_to_minimize, guess, bounds, ftol=ftol, global_max_iter=global_max_iter, 
+                           local_max_iter=local_max_iter, global_ftol_factor=global_ftol_factor, 
+                           enable_global=enable_global, enable_local=enable_local, cma_processes=cma_processes, 
+                           cma_population=cma_population, cma_stds=cma_stds, verbose=verbose, args_dict=minimize_args)
 
         params = res[0]
         params[param_dim:] /= rescale_factor
