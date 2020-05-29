a640dd3b55e2211b2605a9025e2630cae0091eff
Rajesh Singh <rajeshrinet@gmail.com>
2020-05-29 17:47:48 +0100

    refactor integrations in deterministic

--- pyross/deterministic.pyx
+++ pyross/deterministic.pyx
@@ -15,94 +15,6 @@ cdef class IntegratorsClass:
     Integrators used by various deterministic models listed below.
     """
 
-    def _integrate(self, rhs0, x0, Ti, Tf, Nf, integrator, maxNumSteps, **kwargs):
-        """
-        Performs numerical integration
-
-        Parameters
-        ----------
-        rhs0: python function(x,t)
-            Input function of current state and time x, t
-            returns dx/dt
-        x0: np.array
-            Initial state vector (number of compartment values).
-            An array of size M*(model_dimension-1),
-            where x0[i+j*M] should be the initial
-            value of model class i of age group j.
-            The removed R class must be left out.
-            If Ni is dynamical, then the last M points store Ni.
-        Ti: float
-            Start time for integrator.
-        Tf: float
-            End time for integrator.
-        Nf: Int
-            Number of time points to evaluate at.
-        integrator: string, optional
-            Selects which integration method to use. The default is 'odeint'.
-        maxNumSteps: int, optional
-            maximum number of steps the integrator is allowed to take
-            to obtain a solution. The default is 100000.
-        **kwargs: optional kwargs to be passed to the IntegratorsClass
-
-        Raises
-        ------
-        Exception
-            If integration fails.
-
-        Returns
-        -------
-        X: np.array(len(t), len(x0))
-            Numerical integration solution.
-        time_points : np.array
-            Corresponding times at which X is evaluated at.
-
-        """
-
-        if integrator=='solve_ivp':
-            from scipy.integrate import solve_ivp
-            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
-            X = solve_ivp(lambda t, xt: rhs0(xt,t), [Ti,Tf], x0, t_eval=time_points, **kwargs).y.T
-
-        elif integrator=='odeint':
-            from scipy.integrate import odeint
-            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
-            X = odeint(rhs0, x0, time_points, mxstep=maxNumSteps, **kwargs)
-
-        elif integrator=='odespy' or integrator=='odespy-vode':
-            import odespy
-            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
-            solver = odespy.Vode(rhs0, method = 'bdf', atol=1E-7, rtol=1E-6, order=5, nsteps=maxNumSteps)
-            solver.set_initial_condition(x0)
-            X, time_points = solver.solve(time_points, **kwargs)
-
-        elif integrator=='odespy-rkf45':
-            import odespy
-            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
-            solver = odespy.RKF45(rhs0)
-            solver.set_initial_condition(x0)
-            X, time_points = solver.solve(time_points, **kwargs)
-
-        elif integrator=='odespy-rk4':
-            import odespy
-            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
-            solver = odespy.RK4(rhs0)
-            solver.set_initial_condition(x0)
-            X, time_points = solver.solve(time_points, **kwargs)
-
-        else:
-            raise Exception("Error: Integration method not found! \n \
-                            Please set integrator='odeint' to use the scipy's odeint (Default). \n \
-                            Use integrator='odespy-vode' to use vode from odespy (github.com/rajeshrinet/odespy). \n \
-                            Use integrator='odespy-rkf45' to use RKF45 from odespy (github.com/rajeshrinet/odespy). \n \
-                            Use integrator='odespy-rk4' to use RK4 from odespy (github.com/rajeshrinet/odespy). \n \
-                            Alternatively, write your own integrator to evolve the system in time \n")
-        return X, time_points
-
-
-    cpdef set_contactMatrix(self, double t, contactMatrix):
-        self.CM=contactMatrix(t)
-
-
     def simulator(self, x0, contactMatrix, Tf, Nf, integrator='odeint', Ti=0, maxNumSteps=100000, **kwargs):
         """
         Parameters
@@ -139,17 +51,58 @@ cdef class IntegratorsClass:
 
         """
 
-        def rhs0(xt, t):
+        def dxdtEval(xt, t):
             self.CM = contactMatrix(t)
             self.rhs(xt, t)
             return self.dxdt
 
-        X, time_points = self._integrate(rhs0, x0 , Ti, Tf, Nf, integrator, maxNumSteps, **kwargs)
+        if integrator=='odeint':
+            from scipy.integrate import odeint
+            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
+            X = odeint(dxdtEval, x0, time_points, mxstep=maxNumSteps, **kwargs)
+
+        elif integrator=='solve_ivp':
+            from scipy.integrate import solve_ivp
+            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
+            X = solve_ivp(lambda t, xt: dxdtEval(xt,t), [Ti,Tf], x0, t_eval=time_points, **kwargs).y.T
+
+        elif integrator=='odespy' or integrator=='odespy-vode':
+            import odespy
+            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
+            solver = odespy.Vode(dxdtEval, method = 'bdf', atol=1E-7, rtol=1E-6, order=5, nsteps=maxNumSteps)
+            solver.set_initial_condition(x0)
+            X, time_points = solver.solve(time_points, **kwargs)
+
+        elif integrator=='odespy-rkf45':
+            import odespy
+            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
+            solver = odespy.RKF45(dxdtEval)
+            solver.set_initial_condition(x0)
+            X, time_points = solver.solve(time_points, **kwargs)
+
+        elif integrator=='odespy-rk4':
+            import odespy
+            time_points=np.linspace(Ti, Tf, Nf);  ## intervals at which output is returned by integrator.
+            solver = odespy.RK4(dxdtEval)
+            solver.set_initial_condition(x0)
+            X, time_points = solver.solve(time_points, **kwargs)
+
+        else:
+            raise Exception("Error: Integration method not found! \n \
+                            Please set integrator='odeint' to use the scipy's odeint (Default). \n \
+                            Use integrator='odespy-vode' to use vode from odespy (github.com/rajeshrinet/odespy). \n \
+                            Use integrator='odespy-rkf45' to use RKF45 from odespy (github.com/rajeshrinet/odespy). \n \
+                            Use integrator='odespy-rk4' to use RK4 from odespy (github.com/rajeshrinet/odespy). \n \
+                            Alternatively, write your own integrator to evolve the system in time \n")
 
         data     = {'X':X, 't':time_points, 'Ni':self.Ni, 'M':self.M}
         data_out = data.copy()
         data_out.update(self.paramList)
         return data_out
+
+    
+    cpdef set_contactMatrix(self, double t, contactMatrix):
+        self.CM=contactMatrix(t)
 
 
 
