febf4f64182d051bf861a355e84435a81efb6a5f
Rajesh Singh <rajeshrinet@gmail.com>
2020-05-31 16:06:51 +0100

    rename to core

---
+++ core/__init__.py
@@ -0,0 +1,9 @@
+import core.contactMatrix
+import core.control
+import core.deterministic
+import core.forecast
+import core.hybrid
+import core.inference
+import core.stochastic
+import core.utils
+import core.utils_python
