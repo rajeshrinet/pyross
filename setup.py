d23d2a2f87bf1a6cc34364542306944a9a867498
Rajesh Singh <rajeshrinet@gmail.com>
2020-07-16 10:24:44 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -50,8 +50,8 @@ setup(
         ),
     libraries=[],
     packages=['pyross'],
-    install_requires=['cython','numpy','scipy','cma','sympy',
-                      'nlopt==2.6.2','pandas','xlrd'],
+    #install_requires=['cython','numpy','scipy','cma','sympy',
+    #                  'nlopt','pandas','xlrd'],
     package_data={'pyross': ['*.pxd']},
     include_package_data=True,
     classifiers=[
